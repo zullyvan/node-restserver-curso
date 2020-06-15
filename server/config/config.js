2a9b1bfa9a99481e1c7f2d1d9a30835ff443ad16
Zullyvan <Zullyvan@gmail.com>
2020-06-14 19:50:00 -0500

    Variable de entrorno mongo

--- server/config/config.js
+++ server/config/config.js
@@ -19,7 +19,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb+srv://strider:bVTWrggThEoi9Rfm@cluster0-7t0dm.mongodb.net/cafe';
+    urlDB = process.env.MONGO_URI;
 }
 
 process.env.urlDB = urlDB;
